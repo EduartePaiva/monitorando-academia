50fdacacea72c8bcf97ff56f86fc16c72cf1bf85
Eduarte Paiva de Oliveira <74308809+EduartePaiva@users.noreply.github.com>
2023-09-23 07:57:08 -0300

    fazendo a navbar
    a navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
 import { UserButton } from '@clerk/nextjs/app-beta'
 
 export default function Home() {
-  return (
-    <main>
-      <UserButton afterSignOutUrl='/' />
+    return (
+        <main>
+            <nav className='h-12 bg-gray-300 flex justify-between items-center shadow-lg shadow-gray-400'>
+                <div className='flex justify-around w-full text-gray-700 font-semibold h-full'>
+                    <button className='nav'>Exercícios</button>
+                    <button className='nav'>Categorias</button>
+                    <button className='nav'>Registre o dia</button>
+                    <button className='nav'>Minhas estatísticas</button>
+                    <button className='nav'>Configuração</button>
+                </div>
+                <div className='mr-8'>
+                    <UserButton afterSignOutUrl='/' />
 
-    </main>
-  )
+                </div>
+            </nav>
+
+        </main>
+    )
 }
