9c17eceee4e881af7d4c0892d359531b5d52360b
Eduarte Paiva de Oliveira <74308809+EduartePaiva@users.noreply.github.com>
2023-09-24 08:40:16 -0300

    fiz mais um pouco
    fiz um pouco

--- app/(root)/(routes)/exercicios/AddExercicio.tsx
+++ app/(root)/(routes)/exercicios/AddExercicio.tsx
@@ -1,3 +1,7 @@
+'use cllient'
+
+import { useState } from "react"
+
 interface AddExercicioProps {
     diaDaSemana: number
 }
@@ -10,7 +14,24 @@ os dias da semana começam com segunda = 0 e domingo = 6
 
 
 export default function AddExercicio({ diaDaSemana }: AddExercicioProps) {
+    const [opcaoSelecionada, setOpcaoSelecionada] = useState<undefined | "BancoDeDados" | "Personalizado">()
+
+
+
     return (
-        <div>{diaDaSemana}</div>
+        <div className="flex text-center font-semibold">
+            <button
+                className={`flex-1 border-2 p-2 ${opcaoSelecionada === "BancoDeDados" ? "nav-clicked" : "nav"}`}
+                onClick={() => setOpcaoSelecionada("BancoDeDados")}
+            >
+                Adicionar exercício do banco de dados
+            </button>
+            <button
+                className={`flex-1 border-2 p-2 ${opcaoSelecionada === "Personalizado" ? "nav-clicked" : "nav"}`}
+                onClick={() => setOpcaoSelecionada("Personalizado")}
+            >
+                Adicionar exercício personalizado
+            </button>
+        </div>
     )
 }
