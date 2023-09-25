159ead47a8cda15557f98f80f5fa1bd8177408e5
Eduarte Paiva de Oliveira <74308809+EduartePaiva@users.noreply.github.com>
2023-09-25 12:30:26 -0300

    destrinchando o form
    forms

--- app/(root)/(routes)/exercicios/AddExercicioPersonalizado.tsx
+++ app/(root)/(routes)/exercicios/AddExercicioPersonalizado.tsx
@@ -1,7 +1,56 @@
+'use client'
+
+import { Form, useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from 'zod'
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+
+const formSchema = z.object({
+    nome: z.string().min(2, { message: "Mínimo de 2 letras" }).max(20, { message: "O máximo de letras é 300" }),
+    descricao: z.string().min(2, { message: "Mínimo de 2 letras" }).max(300, { message: "O máximo de letras é 300" })
+})
+
 export default function AddExercicioPersonalizado() {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            descricao: "",
+            nome: ""
+        }
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+
     return (
-        <div className="">
-            teste
-        </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+                <FormField
+                    control={form.control}
+                    name='nome'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nome do exercício:</FormLabel>
+                            <FormControl>
+                                <Input placeholder='Nome' {...field} />
+                            </FormControl>
+                            <FormDescription>Ok</FormDescription>
+                            <FormMessage />
+
+                        </FormItem>
+                    )}
+                />
+                <Button type='submit'>Enviar</Button>
+            </form>
+        </Form>
     )
 }
