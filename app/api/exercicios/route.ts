82db04fc8e3007a4046f0c82f82c537dca146e27
Eduarte Paiva de Oliveira <74308809+EduartePaiva@users.noreply.github.com>
2023-09-29 22:37:44 -0300

    mais um pouco
    um pouco

--- app/api/exercicios/route.ts
+++ app/api/exercicios/route.ts
@@ -3,6 +3,42 @@ import { NextResponse } from "next/server"
 import { exercicioFormSchema } from "@/lib/zodSchemas"
 import prismadb from "@/lib/prismadb"
 
+
+//retorna todos os exercícios
+export async function GET(request: Request) {
+    try {
+        const { userId } = auth()
+        if (!userId) return new NextResponse('Unauthorized', { status: 401 })
+
+        const exercicios = await prismadb.exercicio.findMany({
+            where: {
+                userId
+            },
+            select: {
+                id: true,
+                categoriaId: true,
+                imagem_url: true,
+                nome: true,
+                dia_da_semana: true,
+                descricao: true
+            }
+        })
+
+        return NextResponse.json(exercicios.map((exercicio) => (
+            {
+                ...exercicio,
+                id: exercicio.id.toString(),
+                categoriaId: exercicio.categoriaId?.toString()
+            }
+        )))
+
+    } catch (err) {
+        console.log('[EXERCICIOS_GET]', err)
+        return new NextResponse("Internal error", { status: 500 })
+    }
+}
+
+
 export async function POST(request: Request) {
     try {
         const { userId } = auth()
---
+++ types/type.ts
@@ -0,0 +1,11 @@
+
+//exercicios get é um arrei deste
+export interface exercicioGet {
+    id: string;
+    categoriaId: string | undefined;
+    nome: string;
+    descricao: string;
+    dia_da_semana: number;
+    imagem_url: string | null;
+}
+
